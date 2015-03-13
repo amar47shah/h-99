f03ea669de6835b36a7ede20ad6765da3b0dd904
Amar Shah <amar47shah@gmail.com>
2015-03-13 15:21:39 -0400

    Restore previous version of toNativeList

--- 07.hs
+++ 07.hs
@@ -18,7 +18,7 @@ subLists (List xs) = filter isList xs
 toNativeList :: NestedList a -> [a]
 toNativeList  (Elem x ) = [x]
 toNativeList  (List xs)
- | isFlatList (List xs) = concatMap toNativeList xs
+ | isFlatList (List xs) = map (\(Elem x) -> x) xs
  | otherwise            = error "Can't make list from deep-nested."
 
 flatten :: NestedList a -> [a]
