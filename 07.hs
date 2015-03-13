66989df52e74e2a28f10c03762468d07a21c309d
Amar Shah <amar47shah@gmail.com>
2015-03-13 09:58:30 -0400

    Investigate 07
    Definitions for some simpler functions as a lead-up to the solution:
    flattening a nested list.

---
+++ 07.hs
@@ -0,0 +1,22 @@
+data NestedList a = Elem a | List [NestedList a] deriving (Eq, Show)
+
+isElem :: NestedList a -> Bool
+isElem (Elem _) = True
+isElem (List _) = False
+
+isList :: NestedList a -> Bool
+isList = not . isElem
+
+isFlatList :: NestedList a -> Bool
+isFlatList (Elem _ ) = False
+isFlatList (List xs) = and $ map isElem xs
+
+subLists :: NestedList a -> [NestedList a]
+subLists (Elem _ ) = []
+subLists (List xs) = filter isList xs
+
+toNativeList :: NestedList a -> [a]
+toNativeList  (Elem x ) = [x]
+toNativeList  (List xs)
+ | isFlatList (List xs) = map (\(Elem x) -> x) xs
+ | otherwise            = error "Can't make list from deep-nested."
