11b5cde9178774da97aa77512bc08f8cc177cdb9
Amar Shah <amar@shareprogress.org>
2015-04-28 09:16:47 -0400

    Convert `runAndRest` into helper function

--- 09.hs
+++ 09.hs
@@ -1,10 +1,8 @@
 pack :: Eq a => [a] -> [[a]]
 pack xs = fst $ ack ([], xs)
-  where ack (runs, []) = (runs, [])
-        ack (runs, xs) = ack (runs ++ [fst $ runAndRest xs],
-                                       snd $ runAndRest xs)
-
-runAndRest :: Eq a => [a] -> ([a], [a])
-runAndRest (x:xs) = let (  first, second) = span (== x) xs
-                    in  (x:first, second)
-runAndRest _      = ([], [])
+  where ack (runs, [])    = (runs, [])
+        ack (runs, xs)    = ack (runs ++ [fst $ runAndRest xs],
+                                          snd $ runAndRest xs)
+        runAndRest []     = ([], [])
+        runAndRest (x:xs) = (x:run, rest)
+          where (run, rest) = span (== x) xs
