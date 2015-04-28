a495150f7ef45632eed896d65170a50b7a1efd35
Amar Shah <amar@shareprogress.org>
2015-04-28 10:05:59 -0400

    Use `let` instead of `where`, add comments
    Comments describe next strategy for simplifying.

--- 09.hs
+++ 09.hs
@@ -1,6 +1,21 @@
 pack :: Eq a => [a] -> [[a]]
-pack xs = fst $ ack ([], xs)
-  where ack (runs, []) = (runs, [])
-        ack (runs, xs) = ack (runs ++ [run xs], rest xs)
-          where run  (x:xs) = x : takeWhile (== x) xs
-                rest (x:xs) =     dropWhile (== x) xs
+pack xs = let run  (x:xs) = x : takeWhile (== x) xs
+              rest (x:xs) =     dropWhile (== x) xs
+          in  let ack (runs, []) = (runs, [])
+                  ack (runs, xs) = ack (runs ++ [run xs], rest xs)
+              in  fst $ ack ([], xs)
+--still recursing on ack, our accumulator
+--ack is a 2-tuple with two pieces:
+--  the runs found so far
+--      list of lists of items of type a
+--      processed piece
+--  the remaining items
+--      list of items of type a
+--      unprocessed piece
+--
+--do we really need to pass around two pieces?
+--
+--why not:
+--  a list of lists of items of type a
+--  where the last list is unprocessed ...?
+--  [run-one, run-two, ..., unprocessed] <- all items are of type [a]
