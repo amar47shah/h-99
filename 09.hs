d46679fc8b15a0703904e333fe08d9e045a5de39
Amar Shah <amar@shareprogress.org>
2015-04-28 10:31:03 -0400

    Simplify variable name, remove comment

--- 09.hs
+++ 09.hs
@@ -1,7 +1,4 @@
 pack :: Eq a => [a] -> [[a]]
-pack (x:xs) = let (endOfRun, rest) = span (== x) xs
-              in  (x:endOfRun) : pack rest
+pack (x:xs) = let (run, rest) = span (== x) xs
+              in  (x:run) : pack rest
 pack _      = []
---  a list of lists of items of type a
---  where the last list is unprocessed ...?
---  [run-one, run-two, ..., unprocessed] <- all items are of type [a]
