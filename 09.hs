55cb8835ce4d04aeefddf0fd1868b0ea26e4f235
Amar Shah <amar@shareprogress.org>
2015-04-28 10:26:13 -0400

    Simplify recursion

--- 09.hs
+++ 09.hs
@@ -1,21 +1,8 @@
 pack :: Eq a => [a] -> [[a]]
-pack xs = let run  (x:xs) = x : takeWhile (== x) xs
-              rest (x:xs) =     dropWhile (== x) xs
-          in  let ack (runs, []) = (runs, [])
-                  ack (runs, xs) = ack (runs ++ [run xs], rest xs)
-              in  fst $ ack ([], xs)
---still recursing on ack, our accumulator
---ack is a 2-tuple with two pieces:
---  the runs found so far
---      list of lists of items of type a
---      processed piece
---  the remaining items
---      list of items of type a
---      unprocessed piece
---
---do we really need to pass around two pieces?
---
---why not:
+pack (x:xs) = let run  = x : takeWhile (== x) xs
+                  rest =     dropWhile (== x) xs
+              in  run : pack rest
+pack _      = []
 --  a list of lists of items of type a
 --  where the last list is unprocessed ...?
 --  [run-one, run-two, ..., unprocessed] <- all items are of type [a]
