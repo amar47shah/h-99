a0489608fab499811de881897fd64bba3fa31d29
Amar Shah <amar@shareprogress.org>
2015-04-28 10:02:24 -0400

    Restrict scope of helpers

--- 09.hs
+++ 09.hs
@@ -1,8 +1,6 @@
 pack :: Eq a => [a] -> [[a]]
 pack xs = fst $ ack ([], xs)
-  where ack  (runs, []) = (runs, [])
-        ack  (runs, xs) = ack (runs ++ [run xs], rest xs)
-        run  (x:xs)     = x : takeWhile (== x) xs
-        run  _          = [] --for completeness
-        rest (x:xs)     =     dropWhile (== x) xs
-        rest _          = [] --for completeness
+  where ack (runs, []) = (runs, [])
+        ack (runs, xs) = ack (runs ++ [run xs], rest xs)
+          where run  (x:xs) = x : takeWhile (== x) xs
+                rest (x:xs) =     dropWhile (== x) xs
