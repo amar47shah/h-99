b939aa36546a8ef907cd63afa51c087416c110d3
Amar Shah <amar@shareprogress.org>
2015-05-01 23:07:06 -0400

    Reimplement 09 solution with fold

--- 09.hs
+++ 09.hs
@@ -1,4 +1,13 @@
+import Data.List (foldl')
+
 pack :: Eq a => [a] -> [[a]]
 pack (x:xs) = let (run, rest) = span (== x) xs
               in  (x:run) : pack rest
 pack _      = []
+
+pack' :: Eq a => [a] -> [[a]]
+pack' xs = fst $ foldl' step ([], xs) xs
+  where step (runs, xs) x
+          | null run  = (runs         , rest)
+          | otherwise = (runs ++ [run], rest)
+          where (run, rest) = span (== x) xs
