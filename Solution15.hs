acd53c3e5b1640bd6f6a3eb849c03d6fb54a46ac
Amar Shah <amar@shareprogress.org>
2015-06-14 20:55:26 -0400

    Add foldr solution for 15

--- Solution15.hs
+++ Solution15.hs
@@ -2,6 +2,11 @@ module Solution15 where
 
 repli :: (Ord b, Num b) => [a] -> b -> [a]
 repli xs n = concatMap (\x -> multi x n) xs
-    where multi x n
-           | n > 0     = x : multi x (n - 1)
-           | otherwise = []
+
+repli' :: (Ord b, Num b) => [a] -> b -> [a]
+repli' xs n = foldr (\x xs -> multi x n ++ xs) [] xs
+
+multi :: (Ord b, Num b) => a -> b -> [a]
+multi x n
+  | n > 0     = x : multi x (n - 1)
+  | otherwise = []
