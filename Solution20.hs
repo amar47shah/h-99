18a41a07558f4c4fa093f090050668df2585a367
Amar Shah <amar@shareprogress.org>
2015-06-26 21:04:52 -0400

    Solve 20

---
+++ Solution20.hs
@@ -0,0 +1,18 @@
+module Solution20 where
+
+import Control.Arrow ((&&&))
+import Control.Applicative (liftA2)
+
+removeAt :: Int -> [a] -> (a, [a])
+removeAt = liftA2 (&&&) (flip (!!) . pred) skipAt
+
+skipAt :: Int -> [a] -> [a]
+skipAt i xs = (take . pred) i xs ++ drop i xs
+
+--for good measure
+breakAround = liftA2 (&&&) (take . pred) drop
+
+--Pointfree experimenting
+skipAt' i = uncurry (++) . (\n -> liftA2 (&&&) (take . pred) drop n) i
+skipAt''  = \i -> uncurry (++) . (\n -> liftA2 (&&&) (take . pred) drop n) i
+skipAt''' = \i xs -> (take . pred) i xs ++ drop i xs
