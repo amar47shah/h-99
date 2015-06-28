290d7f01bff6a699ef67e126ab46fb6f30d82c4a
Amar Shah <amar@shareprogress.org>
2015-06-28 00:38:11 -0400

    Solve 23 (unsafe)
    Unsafe solution with confusing errors and dangerous behavior:
    * randomSelect "abc" 4: ** Exception: Prelude.(!!): index too large
    * randomSelect "abc" (4): ** Exception: Prelude.(!!): negative index
    * randomSelect "abc" (-1): Completely hangs!

--- Solution23.hs
+++ Solution23.hs
@@ -1,17 +1,53 @@
 module Solution23 where
 
-import System.Random (getStdRandom, randomR)
 import Data.List (group, sort)
+import System.Random (getStdRandom, randomR)
 
+import Solution20 (removeAt)
+
+randomSelect :: [a] -> Int -> IO [a]
+randomSelect _  0 = return []
+randomSelect xs n = do
+  (chosen, remaining) <- randomRemove xs
+  next <- randomSelect remaining (n - 1)
+  return $ chosen : next
+
+randomRemove :: [a] -> IO (a, [a])
+randomRemove xs = do
+  i <- randomPlace xs
+  return $ removeAt i xs
+
+randomPlace :: [a] -> IO Int
+randomPlace xs = getStdRandom . randomR $ (1, length xs)
+
+--Random Elements
+randomElement :: [a] -> IO (Maybe a)
+randomElement xs = do
+  i <- randomIndex xs
+  return $ case i of Nothing -> Nothing
+                     Just i  -> Just $ xs !! i
+
+randomIndex :: [a] -> IO (Maybe Int)
+randomIndex [] = return Nothing
+randomIndex xs = do
+  i <- getStdRandom . randomR $ indexRange xs
+  return $ Just i
+
+indexRange :: [a] -> (Int, Int)
+indexRange = (,) 0 . (pred . length)
+
+--Dice Rolling:
+--an investigation into System.Random
 rolls :: Int -> IO [Int]
 rolls = (mapM $ getStdRandom . randomR) . (dice 6)
 
 dice :: Int -> Int -> [(Int, Int)]
-dice sides n = zip (replicate n 1) (replicate n sides)
+dice sides n = replicate n 1 `zip` replicate n sides
 
 --10 rolls of the dice
 tenRolls :: IO [Int]
-tenRolls = (mapM $ getStdRandom . randomR) $ zip (replicate 10 1) (replicate 10 6)
+tenRolls = rolls 10
+  --(mapM $ getStdRandom . randomR) $ zip (replicate 10 1) (replicate 10 6)
 
 --results of 100 rolls, grouped by result
 resultsOfManyRolls :: Int -> IO [(Int, Int)]
