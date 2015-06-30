c61c14576393d7770cac6cd7940b4181f4131c8f
Amar Shah <amar@shareprogress.org>
2015-06-30 11:04:06 -0400

    Rename formal parameter

--- Solution26.hs
+++ Solution26.hs
@@ -3,20 +3,20 @@ module Solution26 where
 import Data.List (nub, permutations, sort, subsequences)
 
 combinations :: Int -> [a] -> [[a]]
-combinations n xs = filter ((n ==) . length) $ subsequences xs
+combinations k xs = filter ((k ==) . length) $ subsequences xs
 
 combinations' :: (Eq a, Ord a) => Int -> [a] -> [[a]]
-combinations' n = filterTranspositions . permutationsOfLength n
+combinations' k = filterTranspositions . permutationsOfLength k
 
 filterTranspositions :: (Eq a, Ord a) => [[a]] -> [[a]]
 filterTranspositions = nub . map sort
 
 permutationsOfLength :: (Eq a, Ord a) => Int -> [a] -> [[a]]
-permutationsOfLength n xs
-  | n > length xs = []
-  | otherwise     = sort . nub . map (take n) . permutations $ xs
+permutationsOfLength k xs
+  | k > length xs = []
+  | otherwise     = sort . nub . map (take k) . permutations $ xs
 
 combinations'' :: (Eq a, Ord a) => Int -> [a] -> [[a]]
-combinations'' n xs
-  | n > length xs = []
-  | otherwise     = sort . nub . map (sort . take n) . permutations $ xs
+combinations'' k xs
+  | k > length xs = []
+  | otherwise     = sort . nub . map (sort . take k) . permutations $ xs
