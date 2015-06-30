5703801468db42ff6d521b57d52898a1c6204e7e
Amar Shah <amar@shareprogress.org>
2015-06-30 10:25:00 -0400

    Decompose permutation-based solution

--- Solution26.hs
+++ Solution26.hs
@@ -5,9 +5,7 @@ import Data.List (nub, permutations, sort)
 import Solution20 (removeAt)
 
 combinations :: (Eq a, Ord a) => Int -> [a] -> [[a]]
-combinations n xs
-  | n > length xs = []
-  | otherwise     = sort . nub . map (sort . take n) . permutations $ xs
+combinations n xs = undefined
 
 chooseZero :: [a] -> [[a]]
 chooseZero xs = []
@@ -15,3 +13,21 @@ chooseZero xs = []
 chooseOne :: [a] -> [[a]]
 chooseOne xs = map (\i -> (fst $ removeAt i xs) : []) [1..length xs]
                --map (\x -> [x]) xs
+
+--Starting from permutations
+
+combinations' :: (Eq a, Ord a) => Int -> [a] -> [[a]]
+combinations' n xs
+  | n > length xs = []
+  | otherwise     = sort . nub . map (sort . take n) . permutations $ xs
+
+combinations'' :: (Eq a, Ord a) => Int -> [a] -> [[a]]
+combinations'' n = filterTranspositions . permutationsOfLength n
+
+filterTranspositions :: (Eq a, Ord a) => [[a]] -> [[a]]
+filterTranspositions = nub . map sort
+
+permutationsOfLength :: (Eq a, Ord a) => Int -> [a] -> [[a]]
+permutationsOfLength n xs
+  | n > length xs = []
+  | otherwise     = sort . nub . map (take n) . permutations $ xs
