ee5ced15367a34017c80b8cf8eeb08f2e94338c6
Amar Shah <amar@shareprogress.org>
2015-07-26 22:40:28 -0400

    Add specs for Solution02

---
+++ Solution02Spec.hs
@@ -0,0 +1,16 @@
+module Solution01Spec where
+
+import Solution02
+
+import Test.Hspec
+import Test.QuickCheck
+
+main :: IO ()
+main = hspec $ do
+  describe "myButLast" $ do
+    describe "equals last . init" $ do
+      specify "with [Char]" $ property $ (prop :: [Char] -> Property)
+      specify "with [Int]" $ property $ (prop :: [Int] -> Property)
+
+prop :: Eq a => [a] -> Property
+prop xs = (length xs > 1) ==> myButLast xs == (last . init) xs
